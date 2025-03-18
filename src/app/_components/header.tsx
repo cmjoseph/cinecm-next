9ba8d8f2905e4c7239dee710ae0f75ec38752ad0
Claude-Marc Joseph <cmarcjoseph@gmail.com>
2025-03-18 02:12:41 -0400

    mobile menu responsive

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -8,12 +8,30 @@ import { FaFacebookSquare } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { GrInstagram } from "react-icons/gr";
 import { FaSearch } from "react-icons/fa";
+import { VscMenu } from "react-icons/vsc";
+import { GrClose } from "react-icons/gr";
+import { useRef, useLayoutEffect, useState } from "react";
+import { gsap } from "gsap";
 
 export default function Header() {
-    const pathname = usePathname();
+    const pathname                  = usePathname();
+    const [menuOpen, setMenuOpen]   = useState(false);
+    const menuItemsRef              = useRef<HTMLDivElement[]>([]);
+    const toggleClass = (e: any) => {
+        if (menuOpen === false) {
+            setMenuOpen(true);
+            e.target.className = 
+            gsap.fromTo(menuItemsRef.current, { y: -20, opacity: 0 }, { duration: 0.5, y: 0, opacity: 1, stagger: 0.2, ease: 'power3.out' });
+        } else {
+            setMenuOpen(false);
+            gsap.fromTo(menuItemsRef.current, { y: 0, opacity: 1 }, { duration: 0.5, y: -20, opacity: 0, ease: 'power3.out' });
+        }
+    }
+
+    console.log(menuOpen);
     return (
         <header className={styles.header}>
-            <div className={styles.nav}>
+            <div className={`${styles.nav} ${menuOpen ? styles.open : ''}`}>
                 <div className={styles.nav__logo}>
                     <Link href="/" className={styles.logo} style={{ color: variables.primaryColor }}>
                         <Image
@@ -29,17 +47,21 @@ export default function Header() {
                     </Link>
                 </div>
                 <div className={styles.nav__menu}>
-                    <Link className={pathname == "/reviews" ? styles.active : ""} href="/reviews">Reviews</Link>
-                    <Link className={pathname == "/news" ? styles.active : ""} href="/news">News</Link>
-                    <Link className={pathname == "/about" ? styles.active : ""} href="/about">About</Link>
-                    <Link className={pathname == "/contact" ? styles.active : ""} href="/contact">Contact</Link>
+                    <Link ref={(el: any) => { if (menuItemsRef.current) {(menuItemsRef.current[0] = el)} }} className={pathname == "/reviews" ? styles.active : ""} href="/reviews">Reviews</Link>
+                    <Link ref={(el: any) => { if (menuItemsRef.current) {(menuItemsRef.current[1] = el)} }} className={pathname == "/news" ? styles.active : ""} href="/news">News</Link>
+                    <Link ref={(el: any) => { if (menuItemsRef.current) {(menuItemsRef.current[2] = el)} }} className={pathname == "/about" ? styles.active : ""} href="/about">About</Link>
+                    <Link ref={(el: any) => { if (menuItemsRef.current) {(menuItemsRef.current[3] = el)} }} className={pathname == "/contact" ? styles.active : ""} href="/contact">Contact</Link>
                 </div>
-                <div className={styles.nav__socials}>
+                <div className={styles.nav__socials} ref={(el: any) => { if (menuItemsRef.current) {(menuItemsRef.current[4] = el)} }}>
                     <Link href="http://www.facebook.com" target="_blank"><FaFacebookSquare /></Link>
                     <Link href="http://www.x.com" target="_blank"><FaXTwitter /></Link>
                     <Link href="http://www.instagram.com" target="_blank"><GrInstagram /></Link>
                     <Link href="#" onClick={()=> console.log('allo')}><FaSearch /></Link>
                 </div>
+            </div>
+            <div className={`${styles.burger} ${menuOpen ? styles.open : ''}`} onClick={(e) => toggleClass(e)}>
+                <VscMenu className={styles.burger_close} />
+                <GrClose className={styles.burger_open} />
             </div>
         </header>
     );
